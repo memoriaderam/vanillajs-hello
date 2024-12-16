b9e1812741f1c32aa6e431e30f98e40791fa9429
Roberto Agredo <memoriaderam@gmail.com>
2024-12-16 03:56:55 -0400

    Update app.js
    total

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function getColor(colorNumber = 0) {
+	//make sure the parameter is a number and not a string by converting the value to int:
+	colorNumber = parseInt(colorNumber);
+	switch (colorNumber) {
+		case 1: return "red";
+
+		case 2: return "yellow";
+
+		case 3: return "blue";
+
+		case 4: return "green";
+
+		default: return "black";
+
+	}
+}
+
+function getAllStudentColors() {
+
+	//your loop here
+    let exampleColor = getColor(1);
+	//let randomNumber = Math.floor(Math.random()*10);
+	for (randomNumber=0; randomNumber<=10;randomNumber++){
+		let randomNumber = Math.floor(Math.random()*10);
+		console.log(getColor(randomNumber));
+		
+	}
+	return randomNumber;
+
+	//}
+
+}
+
+//call the function below with the number of students in the class and print on the console
+getAllStudentColors();
   console.log("Hello Rigo from the console!");
 };
