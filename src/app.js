dd2c8e55a3c0e0a563f4f895a76f0327f6351596
Roberto Agredo <memoriaderam@gmail.com>
2024-12-16 04:02:10 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,46 +1,81 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  function getColor(colorNumber = 0) {
-	//make sure the parameter is a number and not a string by converting the value to int:
-	colorNumber = parseInt(colorNumber);
-	switch (colorNumber) {
-		case 1: return "red";
-
-		case 2: return "yellow";
-
-		case 3: return "blue";
-
-		case 4: return "green";
-
-		default: return "black";
-
-	}
+function Who (){
+let randomNumberWho = Math.floor(Math.random()*4);
+if (randomNumberWho == 0){
+    console.log(who[0]);
+    return Who = [(who[0])];
+} else if (randomNumberWho == 1){
+    console.log(who[1]);
+    return Who = [(who[1])];
+} else if (randomNumberWho == 2){
+    console.log(who[2]);
+    return Who = [(who[2])];
+} else if (randomNumberWho == 3){
+    console.log(who[3]);
+    return Who = [(who[3])];
 }
+}
+console.log(Who());
 
-function getAllStudentColors() {
+function Action (){
+let randomNumberAction = Math.floor(Math.random()*4);
+if (randomNumberAction == 0){
+    console.log(action[0]);
+    return Action = [(action[0])];
+} else if (randomNumberAction == 1){
+    console.log(action[1]);
+    return Action = [(action[1])];
+} else if (randomNumberAction == 2){
+    console.log(action[2]);
+    return Action = [(action[2])];
+} else if (randomNumberAction == 3){
+    console.log(action[3]);
+    return Action = [(action[3])];
+}
+}
+console.log(Action());
 
-	//your loop here
-    let exampleColor = getColor(1);
-	//let randomNumber = Math.floor(Math.random()*10);
-	for (randomNumber=0; randomNumber<=10;randomNumber++){
-		let randomNumber = Math.floor(Math.random()*10);
-		console.log(getColor(randomNumber));
-		
-	}
-	return randomNumber;
+function What (){
+let randomNumberWhat = Math.floor(Math.random()*3);
+if (randomNumberWhat == 0){
+    console.log(what[0]);
+    return What = [(what[0])];
+} else if (randomNumberWhat == 1){
+    console.log(what[1]);
+    return What = [(what[1])];
+} else if (randomNumberWhat == 2){
+    console.log(what[2]);
+    return What = [(what[2])];
+} 
+}
+console.log(What());
 
+function When (){
+let randomNumberWhen = Math.floor(Math.random()*5);
+if (randomNumberWhen == 0){
+    console.log(when[0]);
+    return When = [(when[0])];
+} else if (randomNumberWhen == 1){
+    console.log(when[1]);
+    return When = [(when[1])];
+} else if (randomNumberWhen == 2){
+    console.log(when[2]);
+    return When = [(when[2])];
+} else if (randomNumberWhen == 3){
+    console.log(when[3]);
+    return When = [(when[3])];
+} else if (randomNumberWhen == 4){
+    console.log(when[4]);
+    return When = [(when[4])];
+}
+}
+console.log(When());
 	//}
 
 }
 
-//call the function below with the number of students in the class and print on the console
-getAllStudentColors();
-  console.log("Hello Rigo from the console!");
-};
+
